5fd65595c00ac057274be1d1449f8c63151e07d1
daniel <daniel.aguiar@outlook.fr>
2024-01-18 11:56:45 +0100

    Remove unnecessary try/catch

--- src/adapters/validators/zod-validator.ts
+++ src/adapters/validators/zod-validator.ts
@@ -12,28 +12,24 @@ export class ZodValidator implements IValidator {
     }
 
     public async validate<T>(payload: Record<string, any>): Promise<Result<T>> {
-        try {
-            const result = this.schema.safeParse(payload);
-            logger.debug(result);
-            if (result.success) {
-                return Result.ok<T>(result.data as T);
-            } else {
-                const validationErrorMessages: Record<string, string[]> = {};
-                result.error.issues.forEach((error: any) => {
-                    const propertyName = error.path[0];
-                    validationErrorMessages[propertyName] = error.message;
-                });
-                const errorMessage = 'Check your payload!';
+        const result = this.schema.safeParse(payload);
+        logger.debug(result);
+        if (result.success) {
+            return Result.ok<T>(result.data as T);
+        } else {
+            const validationErrorMessages: Record<string, string[]> = {};
+            result.error.issues.forEach((error: any) => {
+                const propertyName = error.path[0];
+                validationErrorMessages[propertyName] = error.message;
+            });
+            const errorMessage = 'Check your payload!';
 
-                return Result.fail(
-                    new ValidationError(
-                        errorMessage.replace(/"/g, "'"),
-                        validationErrorMessages
-                    )
-                );
-            }
-        } catch (e: unknown) {
-            throw e;
+            return Result.fail(
+                new ValidationError(
+                    errorMessage.replace(/"/g, "'"),
+                    validationErrorMessages
+                )
+            );
         }
     }
 }
