80c09fb615e0873837c132ce9dc210da70e8df29
daniel <daniel.aguiar@outlook.fr>
2024-01-21 15:24:39 +0100

    Fix(test): create internship use case

--- test/core/use-cases/add-internship.spec.ts
+++ test/core/use-cases/add-internship.spec.ts
@@ -1,6 +1,14 @@
-import { IInternshipRepository } from '../../../src/core/use-cases/interfaces/i-entity-operation';
+import {
+    IAcademicTutorRepository,
+    ICompanyRepository,
+    ICompanyTutorRepository,
+    IInternshipRepository
+} from '../../../src/core/use-cases/interfaces/i-entity-operation';
 import AddInternshipUseCase from '../../../src/core/use-cases/internships/add-internship.use-case';
 import Internship from '../../../src/core/entities/internship.entity';
+import AcademicTutor from '../../../src/core/entities/academic-tutor.entity';
+import Company from '../../../src/core/entities/company.entity';
+import CompanyTutor from '../../../src/core/entities/company-tutor.entity';
 
 describe('Add internship use case', () => {
     class MockInternshipsRepository implements IInternshipRepository {
@@ -21,7 +29,64 @@ describe('Add internship use case', () => {
         }
     }
 
+    class MockAcademicTutorRepository implements IAcademicTutorRepository {
+        save(entity: AcademicTutor): Promise<AcademicTutor> {
+            throw new Error('Method not implemented.');
+        }
+        update(entity: AcademicTutor, id: string): Promise<any> {
+            throw new Error('Method not implemented.');
+        }
+        delete(id: string): Promise<true | null> {
+            throw new Error('Method not implemented.');
+        }
+        findAll(): Promise<AcademicTutor[]> {
+            throw new Error('Method not implemented.');
+        }
+        findOne(id: string): Promise<any> {
+            throw new Error('Method not implemented.');
+        }
+    }
+
+    class MockCompanyRepository implements ICompanyRepository {
+        save(entity: Company): Promise<Company> {
+            throw new Error('Method not implemented.');
+        }
+        update(entity: Company, id: string): Promise<any> {
+            throw new Error('Method not implemented.');
+        }
+        delete(id: string): Promise<true | null> {
+            throw new Error('Method not implemented.');
+        }
+        findAll(): Promise<Company[]> {
+            throw new Error('Method not implemented.');
+        }
+        findOne(id: string): Promise<any> {
+            throw new Error('Method not implemented.');
+        }
+    }
+
+    class MockCompanyTutorRepository implements ICompanyTutorRepository {
+        save(entity: CompanyTutor): Promise<CompanyTutor> {
+            throw new Error('Method not implemented.');
+        }
+        update(entity: CompanyTutor, id: string): Promise<any> {
+            throw new Error('Method not implemented.');
+        }
+        delete(id: string): Promise<true | null> {
+            throw new Error('Method not implemented.');
+        }
+        findAll(): Promise<CompanyTutor[]> {
+            throw new Error('Method not implemented.');
+        }
+        findOne(id: string): Promise<any> {
+            throw new Error('Method not implemented.');
+        }
+    }
+
     const mockInternshipsRepository = new MockInternshipsRepository();
+    const mockAcademicTutorRepository = new MockAcademicTutorRepository();
+    const mockCompanyRepository = new MockCompanyRepository();
+    const mockCompanyTutorRepository = new MockCompanyTutorRepository();
 
     test('should return the saved internship', async () => {
         // Given
@@ -29,7 +94,7 @@ describe('Add internship use case', () => {
 
         const givenValue = {
             studentId: '7bc5f3f1-ed92-464f-9175-e4d264c5d102',
-            companyId: '7bc5f3f1-ed92-464f-9175-k4d264c5d198',
+            companyId: 'LTD',
             companyTutorId: '7bc5f3f1-ed92-464f-9175-e4d264c5d198',
             academicTutorId: '7bc5f3t5-ed92-464f-9175-e4d264c5d102',
             startDate: currentDate,
@@ -41,7 +106,7 @@ describe('Add internship use case', () => {
         const returnedValue = new Internship(
             {
                 studentId: '7bc5f3f1-ed92-464f-9175-e4d264c5d102',
-                companyId: '7bc5f3f1-ed92-464f-9175-k4d264c5d198',
+                companyId: 'LTD',
                 companyTutorId: '7bc5f3f1-ed92-464f-9175-e4d264c5d198',
                 academicTutorId: '7bc5f3t5-ed92-464f-9175-e4d264c5d102',
                 startDate: currentDate,
@@ -52,13 +117,52 @@ describe('Add internship use case', () => {
             '7bc5f3f1-ed92-464f-9175-e4d264c5d104'
         );
 
-        const mock = jest.fn().mockResolvedValue(returnedValue);
+        mockInternshipsRepository.save = jest
+            .fn()
+            .mockResolvedValue(returnedValue);
 
-        mockInternshipsRepository.save = mock;
+        const academicTutorValue = new AcademicTutor({
+            id: '7bc5f3t5-ed92-464f-9175-e4d264c5d102',
+            firstName: 'John',
+            lastName: 'Doe',
+            phoneNumber: '064545545',
+            mailAddress: 'john.doe@gmail.com',
+            schoolEmail: 'john.doe.school@gmail.com'
+        });
+
+        mockAcademicTutorRepository.findOne = jest
+            .fn()
+            .mockResolvedValue(academicTutorValue);
+
+        const companyValue = new Company({
+            name: 'LTD',
+            address: '123 Road Street',
+            city: 'Los Angeles',
+            zipCode: '567'
+        });
+
+        mockCompanyRepository.findOne = jest
+            .fn()
+            .mockResolvedValue(companyValue);
+
+        const companyTutorValue = new CompanyTutor({
+            id: '7bc5f3f1-ed92-464f-9175-e4d264c5d198',
+            firstName: 'John',
+            lastName: 'Doe',
+            phoneNumber: '064545545',
+            mailAddress: 'john.doe@gmail.com'
+        });
+
+        mockCompanyTutorRepository.findOne = jest
+            .fn()
+            .mockResolvedValue(companyTutorValue);
 
         // When
         const result = await new AddInternshipUseCase(
-            mockInternshipsRepository
+            mockInternshipsRepository,
+            mockCompanyRepository,
+            mockAcademicTutorRepository,
+            mockCompanyTutorRepository
         ).perform(givenValue);
 
         // Then
