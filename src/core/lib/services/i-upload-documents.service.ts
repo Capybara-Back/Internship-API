8ed249d34fab3b874a37f39d5d1692006e7343da
daniel <daniel.aguiar@outlook.fr>
2024-02-02 21:00:38 +0100

    chore(s3 upload): retrieve document url

--- src/core/lib/services/i-upload-documents.service.ts
+++ src/core/lib/services/i-upload-documents.service.ts
@@ -1,11 +1,12 @@
 
 export interface DocumentFile {
+    originalName: string;
     name: string;
-    content?: Buffer
+    content?: Buffer;
     type: string;
     path: string;
 }
 
 export interface IUploadDocumentsService {
-    uploadDocuments(files: DocumentFile[]): Promise<any[]>;
+    uploadDocuments(files: DocumentFile[]): Promise<Record<string, string>>;
 }
